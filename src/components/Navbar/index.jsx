a6e7281975dac333d512779d90fce6d7ad7383fc
Ashley Kannemeyer <60740073+TDL2627@users.noreply.github.com>
2023-01-22 22:14:25 +0200

    modal update

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,65 +1,56 @@
-import Link from 'next/link';
-import Image from 'next/image'
-import logo from '../../../public/images/logo.png'
-import Modal from '../elements/Modal';
-import { useState } from 'react';
-import { Login,Register } from '../elements/Forms';
-
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../../../public/images/logo.png";
+import Modal from "../elements/Modal";
+import { useState } from "react";
+import { Login, Register } from "../elements/Forms";
 
 const Navigation = () => {
-    const [showModal,setShowModal]=useState("")
+  const [showModal, setShowModal] = useState("");
   return (
-      <>
-     
-    <nav className="bg-black z-[270] p-2 sticky w-full top-0 flex justify-between items-center">
-    <Link href="/">
-    <Image
-        src={logo}
-        alt="Picture of the author"
-        width={50} 
-        height={50} 
-      />
-    </Link>
-    <div className="flex">
-      <button onClick={()=>{
-          if(showModal){
-            setShowModal("")
-          }else{
-            setShowModal("login")
-          }
-          
-      }}>
-        <p className="text-white font-medium mr-4 cursor-pointer">Login</p>
-      </button>
-      <button onClick={()=>{
-          if(showModal){
-            setShowModal("")
-          }else{
-            setShowModal("register")
-          }
-          
-      }}>
-        <p className="text-white font-medium cursor-pointer">Register</p>
-      </button>
-    </div>
-  </nav>
-{showModal == "login" &&(
     <>
-    <Modal setShowModal={setShowModal} title="login">
-        <Login/>
-    </Modal>
+      <nav className="bg-black z-[270] p-2 sticky w-full top-0 flex justify-between items-center">
+        <Link href="/">
+          <Image
+            src={logo}
+            alt="Picture of the author"
+            width={50}
+            height={50}
+          />
+        </Link>
+        <div className="flex">
+          <button
+            onClick={() => {
+              setShowModal("login");
+            }}
+          >
+            <p className="text-white font-medium mr-4 cursor-pointer">Login</p>
+          </button>
+          <button
+            onClick={() => {
+              setShowModal("register");
+            }}
+          >
+            <p className="text-white font-medium cursor-pointer">Register</p>
+          </button>
+        </div>
+      </nav>
+      {showModal == "login" && (
+        <>
+          <Modal setShowModal={setShowModal} title="login">
+            <Login />
+          </Modal>
+        </>
+      )}
+      {showModal == "register" && (
+        <>
+          <Modal setShowModal={setShowModal} title="register">
+            <Register />
+          </Modal>
+        </>
+      )}
     </>
-)}
-{showModal == "register" &&(
-    <>
-    <Modal setShowModal={setShowModal} title="register">
-        <Register/>
-    </Modal>
-    </>
-)}
-  </>
-
-  )
-}
+  );
+};
 
 export default Navigation;
