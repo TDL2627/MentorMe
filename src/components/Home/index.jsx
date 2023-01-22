f90317f8d7997c1e436850f35684bc852f1a9a5a
Ashley Kannemeyer <60740073+TDL2627@users.noreply.github.com>
2023-01-22 20:09:09 +0200

    first upload

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import Link from 'next/link';
+const Home = () => {
+
+
+  return (
+    <div className="w-full h-full text-center">
+      <div>
+        <h1 className="text-3xl font-bold underline ">Mentor Me</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
