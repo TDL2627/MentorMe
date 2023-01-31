cb310e7865082368a741f3b5abcdb700cd4cd57c
Ashley Kannemeyer <60740073+TDL2627@users.noreply.github.com>
2023-01-31 21:52:03 +0200

    getting somehwer

---
+++ src/components/About/data.js
@@ -0,0 +1,30 @@
+export const aboutDetails = [
+  {
+    title: "WHAT IS THIS APP ABOUT?",
+    content:
+      "This is a first year university student assistance application which is a PWA so it can be installed on mobile devices since students are are mostly on their phones than pc/laptop.This app helps the students transition easier into the university environment. This app is currently soley focused on one field which is Nautical Science.",
+  },
+];
+
+export const featureDetails = [
+  {
+    title: "NOTEPAD",
+    content:
+      "A personal and private notepad that can be accessed on any device any where.",
+  },
+  {
+    title: "MENTOR LIST",
+    content:
+      "A list of mentors with their contact details, description and direct whatsapp links.",
+  },
+  {
+    title: "FEED BACK",
+    content:
+      "A series of questions to check up on students.The responses is then sent to the mentors who can help.",
+  },
+  {
+    title: "GENERAL INFORMATION",
+    content:
+    " A section where students can read relevant and useful information such as study guides, mental health tips and more.",
+  },
+];
--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,13 +1,33 @@
+import { aboutDetails, featureDetails } from "./data";
 const About = () => {
   return (
     <>
-      <div  id="about" className="h-full w-full">
+      <div id="about" className="h-full w-full">
         <div className="w-full py-10   bg-black text-center">
           <p className="text-white text-2xl">About</p>
         </div>
-        <div className="grid grid-cols-2 w-full ">
-          <div className="w-full h-96 bg-gray-600"></div>
-          <div className="w-full h-96 bg-gray-900"></div>
+        <div className="grid md:grid-cols-2 w-full ">
+          <div className="w-full h-96 bg-gray-600">
+            {aboutDetails.map((item) => (
+              <div className="w-full text-start text-white p-10">
+                <h3 className="my-5">{item.title}</h3>
+                <p>{item.content}</p>
+              </div>
+            ))}
+          </div>
+          <div className="w-full h-96 bg-gray-900 overflow-y-auto">
+
+          <div className="w-full text-start text-white p-10">
+          <h3 className="my-5 text-white">FEATURES</h3>
+
+                {featureDetails.map((item) => (
+              <div className="w-full text-start text-white ">
+                <h3 className="my-5">{item.title}</h3>
+                <p>{item.content}</p>
+              </div>
+            ))}
+              </div>
+          </div>
         </div>
       </div>
     </>
