839c74057d6d3d345dcf92cfde56edcda9afeaf3
Ashley Kannemeyer <60740073+TDL2627@users.noreply.github.com>
2023-02-05 02:22:36 +0200

    can add to database sign up

--- src/store/account.js
+++ src/store/account.js
@@ -1,16 +1,27 @@
 import { db } from "../config/firebase.config";
 import { getDoc, doc, setDoc, serverTimestamp } from "firebase/firestore";
 import { create } from "zustand";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  updateEmail,
+  updatePassword,
+} from "firebase/auth";
 
 export const useStore = create((set) => ({
   signUp: async (person) => {
+    const auth = getAuth();
+
     try {
       console.log(person, "aye we here");
-      await setDoc(
-        doc(db, 'users', person.email),
-       person,
-        {merge: true},
+      await createUserWithEmailAndPassword(
+        auth,
+        person.email,
+        person.password,
       );
+      await setDoc(doc(db, "users", person.email), person, { merge: true });
     } catch (error) {
       console.log(error.message, "aye wrong");
     }
